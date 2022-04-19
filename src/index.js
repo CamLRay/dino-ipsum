d5021e8d3f8769884483eaeb4f6f889cbe5ef0d6
Sploob <crayiphone@gmail.com>
2022-04-19 13:12:43 -0700

    create dino hangman

--- src/index.js
+++ src/index.js
@@ -2,3 +2,58 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoIpsum from './js/dino-ipsum';
+import DinoHangman from './js/dino-hangman';
+
+let newDino = new DinoHangman();
+
+let promise = DinoIpsum.getDinos();
+promise.then(function(response) {
+  const dinoName = JSON.parse(response); 
+  newDino.addDino(dinoName[0][0]);
+  $("#start-game").removeClass("hidden");
+},  function(error) {
+  $("#displayError").text(`There was an error ${error}`);
+});
+$("#start-game").click(function(){
+  $("#start-game").addClass("hidden");
+  $(".start-game").removeClass("hidden");
+  newDino.updateCurrentState();
+  $("#displayName").text(newDino.currentState.join(" "));
+  $("#chances").text(newDino.tries);
+});
+
+
+
+$("form").submit(function(e){
+  e.preventDefault();
+  let letter = $("input").val();
+  $("input").val("");
+  newDino.guess(letter);
+  $("#displayName").text(newDino.currentState.join(" "));
+  $("#chances").text(`${newDino.tries} Incorrect Guesses: ${newDino.incorrectGuess}`);
+  if(!newDino.currentState.includes("_")){
+    $(".start-game").addClass("hidden");
+    $("#displayName").text(`Winner you guessed it right the dinosaur was ${newDino.dino}!`);
+    $('#play-again').removeClass("hidden");
+  }
+  if (newDino.tries < 0) {
+    $(".start-game").addClass("hidden");
+    $('#play-again').removeClass("hidden");
+    $("#displayName").text(`You Lose, you couldn't guess ${newDino.dino}. What a loser!`);
+  }
+});
+
+$('#play-again').click(function(){
+  newDino = new DinoHangman();
+  let promise = DinoIpsum.getDinos();
+  promise.then(function(response) {
+    const dinoName = JSON.parse(response); 
+    newDino.addDino(dinoName[0][0]);
+    $("#start-game").removeClass("hidden");
+  },  function(error) {
+    $("#displayError").text(`There was an error ${error}`);
+  });
+  $("#displayName").text("");
+  $('#play-again').addClass("hidden");
+});
---
+++ src/js/dino-hangman.js
@@ -0,0 +1,34 @@
+export default class DinoHangman {
+  constructor(){
+    this.tries = 6;
+    this.incorrectGuess = "";
+  }
+
+  addDino(dino){
+    this.dino = dino;
+  }
+  updateCurrentState(){
+    this.currentState = [];
+    for(let i = 0; i < this.dino.length; i++) {
+      this.currentState.push('_');
+    }
+  }
+
+  guess(letter) {
+    let indexes = [];
+    for(let i =0; i < this.dino.length; i++) {
+      if(this.dino[i].toLowerCase() === letter.toLowerCase()) {
+        indexes.push(i);
+      }
+    }
+    if(indexes.length === 0){
+      this.tries--;
+      this.incorrectGuess += letter;
+    } else {
+      indexes.forEach((index) => {
+        this.currentState[index] = letter;
+        this.currentState[0] = this.currentState[0].toUpperCase();
+      });
+    }
+  }
+}
